fd3b3c66000f15efd2dfb2e47aac418279890f31
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2022-03-05 15:36:16 +0300

    Gerçek Zamanlı Yüz Tespiti (Real Time Face Detection)

---
+++ Gerçek Zamanlı Yüz Tespiti (Real Time Face Detection).py
@@ -0,0 +1,32 @@
+
+
+import cv2
+import matplotlib.pyplot as plt
+
+
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+
+while True:
+    ret,frame=cap.read()
+    frame = cv2.flip(frame,1)
+    if ret==False:
+        break
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    
+    
+    
+    faces = face_cascade.detectMultiScale(gray,1.3,2)
+    
+    
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h ),(0,255,0),4)
+        
+    cv2.imshow("Detected Faces",frame)
+    if cv2.waitKey(30) & 0xFF == ord("q"):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+
